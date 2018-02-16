419f0c17124c554ca5e404741fe656e978c5348d
geoff567 <31115482+geoff567@users.noreply.github.com>
2018-02-15 19:18:15 -0800

    update

---
+++ app.js
@@ -0,0 +1,50 @@
+var csvUrl = "https://raw.githubusercontent.com/Peleke/geo-plots/master/list_aavso-arssn_yearly.txt";
+Plotly.d3.csv(csvUrl, function(err, data) {
+  function unpack(rows, key) {
+    return rows.map(row => row[key]);
+  }
+
+  // @Objective
+  function lag(data, tau = 1) {
+    if (tau >= data.length) {
+      return [];
+    } else {
+      return data.slice(tau);
+    }
+  }
+
+  // @Objective
+  var year = unpack(data, "year");
+  var wolfNumber = unpack(data, "wolf_number");
+  var wolfNumberLag = lag(wolfNumber, 1);
+
+  // @Objective
+  var data = [{
+    x: year,
+    y: wolfNumber,
+    mode: "markers",
+    type: "scatter"
+  }];
+
+  // @Objective
+  var lag =[{
+    x: wolfNumber,
+    y: wolfNumberLag,
+    mode: "markers",
+    type: "scatter"
+  }];
+
+  // @Objective
+  var dataLayout = {
+    title: "Wolf Number vs Year"
+  };
+
+  // @Objective
+  var lagLayout = {
+    title: "Wolf Number Lag (τ=1)"
+  };
+
+  // @Objective
+  Plotly.plot("wolf-number-scatter", data, dataLayout);
+  Plotly.plot("wolf-number-lag", lag, lagLayout);
+});
